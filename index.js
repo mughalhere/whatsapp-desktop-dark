a0c89015fe06febe9aa3c09c9a84393af6cde5c8
Amir Mughal <mughalheree@gmail.com>
2019-09-05 23:05:37 +0500

    Updated the Main Screen Title As Well

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-console.log('\x1b[40m\x1b[4m\x1b[1m\x1b[36m%s\x1b[0m', '\n~~~~ WhatsApp Desktop Dark Mode by m4heshd ~~~~\n\n');
+console.log('\x1b[40m\x1b[4m\x1b[1m\x1b[36m%s\x1b[0m', '\n~~~~ WhatsApp Desktop Dark Mode by CG <3 ~~~~\n\n');
 
 switch (process.platform) {
     case 'win32':
